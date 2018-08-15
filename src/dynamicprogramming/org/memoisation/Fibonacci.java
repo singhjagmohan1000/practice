45070269b31c1f490b5c3a31b63f38c3016d2ccc
Jagmoah Singh <singhjagmohan4734@gmail.com>
2018-08-14 20:07:22 -0700

    fibonacci with memoization

--- src/dynamicprogramming/org/memoisation/Fibonacci.java
+++ src/dynamicprogramming/org/memoisation/Fibonacci.java
@@ -1,17 +1,28 @@
 package dynamicprogramming.org.memoisation;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * Created by Jagmohan Singh on 8/14/18.
  */
 public class Fibonacci {
 
     public static int  fib(int n){
+        int result=0;
 
-        if(n == 0)
-            return 0;
-        else if (n<=2 )
-            return 1;
-        else return fib(n-1) + fib(n-2);
+        HashMap<Integer,Integer> memo = new HashMap<Integer,Integer>();
+
+        if (n<=1 )
+            return n;
+        else {
+            if(memo.containsKey(n))
+                return memo.get(n);
+            else
+            result =  fib(n-1) + fib(n-2);
+            memo.put(n,result);
+            return result;
+        }
 
 
 
